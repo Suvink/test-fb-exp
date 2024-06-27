71b0cad3f8f432defd797351d5866a96d16b21f3
Suvin Nimnaka <suvin@wso2.com>
2024-06-27 20:03:45 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const app = express();
+const port = 8000;
+const bodyParser = require("body-parser");
+
+const serviceAccount = require("./uom-session-completed-firebase-adminsdk-xa60n-d6d20a6502.json");
+app.use(bodyParser.json());
+initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: "uom-session-completed.firebaseio.com"
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get("/people", (req, res) => {
+    const db = getFirestore();
+    const items = db.collection("people");
+    items.get().then((querySnapshot) => {
+        const itemsArray = [];
+        querySnapshot.forEach((doc) => {
+            itemsArray.push(doc.data());
+        });
+        res.send(itemsArray);
+    });
+});
+
+app.post("/people", (req, res) => {
+    console.log(req.body)
+    const db = getFirestore();
+    const items = db.collection("people");
+    items.add(req.body).then(() => {
+        res.send("Document successfully written!");
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
